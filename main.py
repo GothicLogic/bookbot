28ab76ef41605c0de9e60a6c8f28e6c2988f4cfe
GothicLogic <iammorski@gmail.com>
2025-07-31 03:46:30 +0100

    Create Bookbot

---
+++ main.py
@@ -0,0 +1,25 @@
+from stats import count_words
+from stats import count_chars
+from stats import sort_dict
+import sys
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+def main():
+     if len(sys.argv) < 2:
+         print("Usage: python3 main.py <path_to_book>")
+         sys.exit(1)
+     path = sys.argv[1]
+     text = get_book_text(path)
+     print("============ BOOKBOT ============")
+     print(f"Analyzing book found at {path}")
+     print("----------- Word Count ----------")
+     count_words(text)
+     print("--------- Character Count -------")
+     char_dict = count_chars(text)
+     sort_dict(char_dict)
+     print("============= END ===============")
+
+main()
